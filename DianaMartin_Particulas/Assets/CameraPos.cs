efd7f9e015f1cb2b64f034ade3c5e6bc9af610c5
Diana Martin <dianamartin.cuadern@gmail.com>
2020-01-12 21:26:28 +0100

    Added Script & button Canvas working

---
+++ DianaMartin_Particulas/Assets/CameraPos.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPos : MonoBehaviour
+{
+    
+    public GameObject[] pos;
+    public int index = 0;
+    
+
+
+
+    public void NextParticle(){
+        index++;
+
+        if (index >= pos.Length)
+        {
+            index = 0;
+        }
+
+        transform.position = pos[index].transform.position;
+        transform.rotation = pos[index].transform.rotation;
+    }
+}
